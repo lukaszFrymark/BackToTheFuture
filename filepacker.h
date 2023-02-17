8002c132cf2980d25c22511e5d3794d5af3ec462
Łukasz Frymark <lukasz.frymark@gmail.com>
2023-02-17 18:45:28 +0100

    Dev in progress
    * Hashing function
    * Cobining files without added directory tree info
    * Check for unique files
    * Vector storing info of file path, size and hash.

--- filepacker.h
+++ filepacker.h
@@ -3,6 +3,16 @@
 #include <QtCore>
 #include "progressticks.h"
 
+class fileInfo
+{
+public:
+    fileInfo( QString path, qint64  size, QString hash );
+    fileInfo();
+    QString path;
+    qint64  size;
+    QString hash;
+};
+
 class filePacker
 {
 public:
@@ -10,10 +20,16 @@ public:
     void pack(char* filePath, char* outPath);
     void unPack(char* filePath, char* outDirPath);
     bool validateFilePath(char* filePath);
+    bool validateFilePath(const QString& filePath);
     bool validateDirPath(char* dirPath);
+    QString fileChecksum(const QString& fileName, QCryptographicHash::Algorithm hashAlgorithm);
+    qint64 appendFileToResult( const QString& fileName, QFile& resultFile );
+    bool uniqueFile(const QString& hash);
 
 public:
     ProgressTicks* tick;
+    QString blobFileName;
+    QVector<fileInfo> filesDesc;
 };
 
 #endif // FILEPACKER_H
