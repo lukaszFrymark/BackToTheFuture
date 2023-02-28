124ea10c913e39c5c9013ffe2204f186b3e44508
Łukasz Frymark <lukasz.frymark@gmail.com>
2023-02-28 12:07:31 +0100

    Code formatting acording to "Allman Coding Style". Member objects allocated  on the stack.

--- main.cpp
+++ main.cpp
@@ -5,30 +5,30 @@
 void helpMsg(char* progName)
 {
     qInfo() << progName << "[options]" << '\n' <<
-        "Options:" << '\n' <<
-        "-h | --help                                        Print this help" << '\n' <<
-        "-p | --pack {directory_path} {output.path}         Pack files from directory" << '\n' <<
-        "-u | --unpack {packed_logs_path} {output.path}     Unpack file into given direcory path" << '\n'
-        ;
+            "Options:" << '\n' <<
+            "-h | --help                                        Print this help" << '\n' <<
+            "-p | --pack {directory_path} {output.path}         Pack files from directory" << '\n' <<
+            "-u | --unpack {packed_logs_path} {output.path}     Unpack file into given direcory path" << '\n'
+            ;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    filePacker fp;
+    FilePacker fp;
 
-    if(argc == 1 || QString(argv[1]).compare("-h") == 0)    //No args or '-h'
+    if (argc == 1 || QString(argv[1]).compare("-h") == 0)   //No args or '-h'
     {
         helpMsg(argv[0]);
         return 0;
     }
-    else if(argc > 3 && (QString(argv[1]).compare("-p") == 0 || QString(argv[1]).compare("-pack") == 0 ))   // -p or -pack with path
+    else if (argc > 3 && (QString(argv[1]).compare("-p") == 0 || QString(argv[1]).compare("-pack") == 0))   // -p or -pack with path
     {
-        fp.pack( argv[2], argv[3] );
+        fp.pack(argv[2], argv[3]);
     }
 
-    else if(argc > 3 && (QString(argv[1]).compare("-u") == 0 || QString(argv[1]).compare("-unpack") == 0 ))   // -u or -unpack with paths
+    else if (argc > 3 && (QString(argv[1]).compare("-u") == 0 || QString(argv[1]).compare("-unpack") == 0))   // -u or -unpack with paths
     {
-        fp.unPack( argv[2], argv[3] );
+        fp.unPack(argv[2], argv[3]);
     }
     else
     {
